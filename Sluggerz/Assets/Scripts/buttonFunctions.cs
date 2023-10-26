47361adeee581c3008e2d148b12083063d2b5961
NesaWatson <nkwatson@student.fullsail.edu>
2023-10-25 21:58:22 -0400

    updated button functions

--- Sluggerz/Assets/Scripts/buttonFunctions.cs
+++ Sluggerz/Assets/Scripts/buttonFunctions.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void resume()
     {
-        
+        gameManager.instance.stateUnpause();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void restart()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameManager.instance.stateUnpause();
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+    //public void givePlayerHP(int amount)
+    //{
+    //    gameManager.instance.playerScript.giveHP(amount);
+    //}
+    public void respawnPlayer()
+    {
+        gameManager.instance.stateUnpause();
+        gameManager.instance.playerScript.spawnPlayer();
     }
 }
+
