39296a5ef78de333f49d63b92c02c178f5ce0fd5
NesaWatson <nkwatson@student.fullsail.edu>
2023-10-29 12:43:22 -0400

    Readded assets

--- Sluggerz/Assets/Scripts/playerController.cs
+++ Sluggerz/Assets/Scripts/playerController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Experimental.AI;
 
 public class playerController : MonoBehaviour, iDamage, iPhysics
 {
